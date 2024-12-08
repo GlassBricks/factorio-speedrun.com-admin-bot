4da0d5c4d63c5aea638848ea3883edd5c1277703
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2024-12-07 19:25:04 -0600

    Edit server setup scripts

--- restart-server.sh
+++ restart-server.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 pm2 stop bot
+npm ci
 npm run clean
 npm run build
-pm2 start ./dist/main.js --name bot --watch
+pm2 start ./dist/main.js --name bot
--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+npm ci
 pm2 install pm2-logrotate
